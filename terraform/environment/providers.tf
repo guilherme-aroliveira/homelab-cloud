2f6517c41e03c3977f0ff89725f8aec43adbec4d
Guilherme Oliveira <oliveira.g.ar@proton.me>
2025-07-17 16:26:27 -0300

    update providers.tf file

--- terraform/environment/providers.tf
+++ terraform/environment/providers.tf
@@ -1,6 +1,6 @@
 provider "digitalocean" {
-  token = var.do_token
-  spaces_access_id = var.spaces_key
+  token             = var.do_token
+  spaces_access_id  = var.spaces_key
   spaces_secret_key = var.spaces_secret
 }
 
