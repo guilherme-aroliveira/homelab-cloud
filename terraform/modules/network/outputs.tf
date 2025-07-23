d1e0315ff2b2c073278f5f325db1f70f8cfbe0d7
Guilherme Oliveira <oliveira.g.ar@proton.me>
2025-07-23 17:05:26 -0300

    fmt terraform configuration

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "VPC ID for droplets"
-  value = digitalocean_vpc.main_vpc.id
+  value       = digitalocean_vpc.main_vpc.id
 }
 
 output "jenkins_ip" {
   description = "Jenkins server static IP"
-  value = digitalocean_reserved_ip.jenkins_static_ip.ip_address
+  value       = digitalocean_reserved_ip.jenkins_static_ip.ip_address
 }
 
 output "firezone_ip" {
   description = "Firezone vpn static IP"
-  value = digitalocean_reserved_ip.firezone_static_ip.ip_address
+  value       = digitalocean_reserved_ip.firezone_static_ip.ip_address
 }
